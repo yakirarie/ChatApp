f3ee0ecf8de460130b6a2402078d5f310d6b5fd6
yakirarie <yakirarie4@gmail.com>
2020-03-22 12:39:05 +0200

    added firebase

--- app/src/main/java/com/yakirarie/chatapp/CreateUserActivity.kt
+++ app/src/main/java/com/yakirarie/chatapp/CreateUserActivity.kt
@@ -1,25 +1,111 @@
 package com.yakirarie.chatapp
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    private val TAG = "CreateUserActivityDebug"
+    private var selectedPhotoUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
-    fun generateUserAvatar(view: View) {
-
-    }
-
-    fun generateColorClicked(view: View) {
-
-    }
 
     fun createUserClicked(view: View) {
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                uploadImageToFirebaseStorage()
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    fun selectPhotoClicked(view: View) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            profileCircleImageView.setImageBitmap(bitmap)
+            defaultProfileImageView.alpha = 0f
+
+        }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if (selectedPhotoUri == null) {
+            Toast.makeText(this, "Please add a profile image", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!).addOnSuccessListener {
+            Log.d(TAG, "Successfully uploaded image: ${it.metadata?.path}")
+            ref.downloadUrl.addOnSuccessListener {
+                Log.d(TAG, "File location: $it")
+                saveUserToFirebaseDatabase(it.toString())
+
+            }
+        }.addOnFailureListener {
+            Log.d(TAG, "Failed to upload Image: ${it.message}")
+            Toast.makeText(this, "Failed to upload Image: ${it.message}", Toast.LENGTH_SHORT)
+                .show()
+        }
 
     }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseFirestore.getInstance().collection("users")
+        val user = User(uid, createUserNameText.text.toString(), profileImageUrl)
+        ref.add(user).addOnSuccessListener {
+            Log.d(TAG, "Successfully saved user to Firestore!")
+            Toast.makeText(this, "Successfully saved user to database!", Toast.LENGTH_SHORT)
+                .show()
+
+        }.addOnFailureListener {
+            Log.d(TAG, "Failed to save user to Firestore: ${it.message}")
+            Toast.makeText(
+                this,
+                "Failed to save user to database: ${it.message}",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
 }
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
--- app/src/main/java/com/yakirarie/chatapp/LoginActivity.kt
+++ app/src/main/java/com/yakirarie/chatapp/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,6 +16,21 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to login: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
 
     }
 
