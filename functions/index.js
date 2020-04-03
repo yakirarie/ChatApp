dac78fc891c15144f2545a5b3a3a832954a3dfb2
yakirarie <yakirarie4@gmail.com>
2020-04-03 15:39:17 +0300

    added notification for new group

--- functions/index.js
+++ functions/index.js
@@ -60,9 +60,9 @@ exports.newMessageTwoUsers = functions.database.ref('/latest-messages/{id1}/{id2
         }
         try {
             const response = await admin.messaging().sendToDevice(recieverUser.token, payload);
-            return console.log("Successfully sent message:", response);
+            console.log("Successfully sent message:", response);
         } catch (error) {
-            return console.log("Error sending message:", error);
+            console.log("Error sending message:", error);
         }
     });
 
@@ -86,12 +86,6 @@ exports.newMessageGroup = functions.database.ref('/group-messages/{groupId}/{mes
         const snapGroup = await admin.database().ref("/users/" + context.params.groupId).once('value');
         const group = snapGroup.val();
 
-        // const recieversUsersPromises = []
-        // for (const rec of receiversIds) {
-        //     recieversUsersPromises.push(admin.database().ref("/users/" + rec).once('value'));
-        // }
-        // const recieversUsersSnapshots = await Promise.all(recieversUsersPromises);
-
         const payload = {
 
             notification: {
@@ -105,7 +99,7 @@ exports.newMessageGroup = functions.database.ref('/group-messages/{groupId}/{mes
                 sender_token: senderUser.token,
 
                 group_id: group.uid,
-                group_admin_uid: group.groupAdmin,
+                group_admin_uid: group.groupAdminUID,
                 group_name: group.groupName,
                 group_image: group.groupImageUrl,
                 group_size: group.usersList.length.toString()
@@ -120,13 +114,7 @@ exports.newMessageGroup = functions.database.ref('/group-messages/{groupId}/{mes
             payload.data[`receiver_img${i+1}`] = group.usersList[i].profileImageUrl;
             payload.data[`receiver_token${i+1}`] = group.usersList[i].token;
         }
-        // for (let i = 0; i < recieversUsersSnapshots.length; i++) {
-        //     payload.data[`receiver_id${i+1}`] = recieversUsersSnapshots[i].val().uid;
-        //     payload.data[`receiver_username${i+1}`] = recieversUsersSnapshots[i].val().username;
-        //     payload.data[`receiver_img${i+1}`] = recieversUsersSnapshots[i].val().profileImageUrl;
-        //     payload.data[`receiver_token${i+1}`] = recieversUsersSnapshots[i].val().token;
 
-        // }
         // if (chatMessage.messageType === "image") {
         //     payload.notification.body = "";
         //     payload.notification.image = messageText;
@@ -136,17 +124,75 @@ exports.newMessageGroup = functions.database.ref('/group-messages/{groupId}/{mes
         //     payload.notification.body = "🎥 video file 🎥";
 
         // }
+
         const responses = []
         for (const user of group.usersList) {
-            if (senderId !== user.uid){
+            if (senderId !== user.uid) {
                 try {
                     const response = admin.messaging().sendToDevice(user.token, payload);
                     responses.push(response)
                 } catch (error) {
-                    console.log("Error sending message to "+user.username+":", error);
+                    console.log("Error sending message to " + user.username + ":", error);
                 }
             }
-            
+
+        }
+        const responses_results = await Promise.all(responses);
+        for (const res of responses_results)
+            console.log("Successfully sent message", res);
+
+
+    });
+
+// listen for new group creation
+exports.newGroupCreation = functions.database.ref('/users/{groupId}')
+    .onWrite(async (change, context) => {
+
+        const newGroup = change.after.val();
+        if (newGroup.groupName === null) return
+        const adminUser = newGroup.usersList.filter(user => user.uid === newGroup.groupAdminUID)[0];
+
+        const payload = {
+
+            notification: {
+                title: adminUser.username + " Created " + newGroup.groupName,
+                body: "You, " + adminUser.username + " and " + newGroup.usersList.length.toString() + " more"
+            },
+            data: {
+                sender_id: adminUser.uid,
+                sender_username: adminUser.username,
+                sender_img: adminUser.profileImageUrl,
+                sender_token: adminUser.token,
+
+                group_id: newGroup.uid,
+                group_admin_uid: newGroup.groupAdminUID,
+                group_name: newGroup.groupName,
+                group_image: newGroup.groupImageUrl,
+                group_size: newGroup.usersList.length.toString()
+
+            }
+
+        };
+
+        for (let i = 0; i < newGroup.usersList.length; i++) {
+            payload.data[`receiver_id${i+1}`] = newGroup.usersList[i].uid;
+            payload.data[`receiver_username${i+1}`] = newGroup.usersList[i].username;
+            payload.data[`receiver_img${i+1}`] = newGroup.usersList[i].profileImageUrl;
+            payload.data[`receiver_token${i+1}`] = newGroup.usersList[i].token;
+        }
+
+
+        const responses = []
+        for (const user of newGroup.usersList) {
+            if (newGroup.groupAdminUID !== user.uid) {
+                try {
+                    const response = admin.messaging().sendToDevice(user.token, payload);
+                    responses.push(response)
+                } catch (error) {
+                    console.log("Error sending message to " + user.username + ":", error);
+                }
+            }
+
         }
         const responses_results = await Promise.all(responses);
         for (const res of responses_results)
